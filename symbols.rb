24ff3a026fd646dcc5352c888902b832a4963b17
Shobhit Agarwal <shobhit657@gmail.com>
2017-04-11 10:32:35 -0500

    start gc

--- symbols.rb
+++ symbols.rb
@@ -1,2 +1,16 @@
-puts :'I love ruby'
-puts :'I love Ruby.'.object_id
+# Symbol GC test
+require 'java'
+
+java_import 'java.lang.System'
+
+before = Symbol.all_symbols.size
+
+
+100_000.times do |i|
+  "sym#{i}".to_sym
+end
+
+# GC.start
+System.gc
+after = Symbol.all_symbols.size
+puts after - before
